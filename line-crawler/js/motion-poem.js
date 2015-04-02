4ef86e6c570ae4753f28ac0457e056d17a58f270
John Fraboni <johnfraboni@hotmail.com>
2015-04-02 02:44:22 +0000

    * add complete line crawler

--- line-crawler/js/motion-poem.js
+++ line-crawler/js/motion-poem.js
@@ -13,12 +13,15 @@
         const UP = 'up';
 
         // TODO 1 : Declare our variables //
-        
+        var i, shape, direction;
 
         var _motionPoem = {
             init: function() {
                 // TODO 2 : Initialize our variables //
-                
+                i = 0;
+                direction = ACROSS;
+                shape = new createjs.Shape();
+                view.addChild(shape);
             },
 
             update: function () {
@@ -29,17 +32,49 @@
                 ////////////////////////////////////////////////////////////////
                 
                 // TODO 3 : Create the condition for ACROSS //
-                
+                if (direction === ACROSS) {
+                    draw.line(i, 0, Math.random() * canvas.width, canvas.height, draw.randomColor(25, 200, 255, .4), 7, shape);
+                    if (i < canvas.width) {
+                        i++;
+                    } else {
+                        direction = DOWN;
+                        i = 0;
+                    }
+                } 
 
                 // TODO 4 : Create a condition for DOWN //
-                 
-                
+                else if (direction === DOWN) {
+                    draw.line(canvas.width, i, 0, Math.random() * canvas.height, draw.randomColor(50, 255, 255, .3), 7, shape);
+                    if (i < canvas.height) {
+                        i++;
+                    } else {
+                        direction = BACK;
+                        i = canvas.width;
+                    }
+                } 
+
                 // TODO 5 : Create a condition for BACK //
-                
-                 
+                else if (direction === BACK) {
+                    draw.line(i, canvas.height, Math.random() * canvas.width, 0, draw.randomColor(50, 200, 255, .2), 7, shape);
+                    if (i > 0) {
+                        i--;
+                    } else {
+                        direction = UP;
+                        i = canvas.height;
+                    }
+                } 
+
                 // TODO 6 : Create a condition for UP //
-                
-                
+                else {
+                    draw.line(0, i, canvas.width, Math.random() * canvas.height, draw.randomColor(50, 255, 200, .4), 7, shape);
+                    if (i > 0) {
+                        i--;
+                    } else {
+                        direction = ACROSS;
+                        i = 0;
+                        shape.graphics.clear();
+                    }
+                }
                 ////////////////////////////////////////////////////////////////
                 // END OF CONDITIONAL STATEMENTS                              //
                 ////////////////////////////////////////////////////////////////
